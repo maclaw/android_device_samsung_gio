31a76af6c52d1de4cd94d808b828ae31ccfecad1
Maciej Maj <maclaw@maclaw.pl>
2011-12-26 11:38:14 +0100

    ext4 support fixed

--- Android.mk
+++ Android.mk
@@ -21,12 +21,5 @@ LOCAL_SRC_FILES := prebuilt/sec_touchscreen.kcm
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_KEY_CHAR_MAP)
 
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := setup_fs.c
-LOCAL_MODULE := setup_fs
-LOCAL_MODULE_TAGS := optional
-#LOCAL_SHARED_LIBRARIES += libext4_utils libz
-include $(BUILD_EXECUTABLE)
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
